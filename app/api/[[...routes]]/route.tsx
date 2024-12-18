fee53e356810b74bf17a0e8b3f1dd410cf58db66
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2024-12-17 17:47:25 -0800

    feat: highlight found words and fix word check

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -6,6 +6,7 @@ import { devtools } from 'frog/dev';
 import { handle } from 'frog/next';
 import { serveStatic } from 'frog/serve-static';
 import { Roboto } from '../styles/fonts';
+import { container, highlight } from '../styles/styles';
 
 const app = new Frog({
   assetsPath: '/',
@@ -20,6 +21,45 @@ const app = new Frog({
     fonts: [...Roboto],
   },
 });
+
+const highlightStyle = {
+  ethereum: {
+    top: 306,
+    left: 443,
+    width: 310,
+    height: 34,
+    background: 'rgba(0,200,0,0.4)',
+  },
+  popcorn: {
+    top: 234,
+    left: 360,
+    width: 359,
+    height: 34,
+    background: 'rgba(0,120,250,0.4)',
+    transform: 'rotate(44deg)',
+  },
+  token: {
+    top: 230,
+    left: 406,
+    width: 36,
+    height: 185,
+    background: 'rgba(96, 0, 250, 0.4)',
+  },
+  wallet: {
+    top: 156,
+    left: 795,
+    width: 35,
+    height: 222,
+    background: 'rgba(250,0,0,0.4)',
+  },
+  swap: {
+    top: 379,
+    left: 523,
+    width: 36,
+    height: 148,
+    background: 'rgba(255, 238, 0, 0.4)',
+  },
+};
 
 function foundWord(word: string) {
   let isNew = !foundWords.includes(word);
@@ -37,7 +77,7 @@ const wordList = ['ethereum', 'popcorn', 'swap', 'token', 'wallet'];
 const foundWords: string[] = [];
 app.frame('/', c => {
   const { inputText, status } = c;
-  const word = inputText ?? '';
+  const word = (inputText ?? '').toLowerCase();
   let found = wordList.includes(word);
   return c.res(
     status === 'response' && word !== ''
@@ -46,7 +86,41 @@ app.frame('/', c => {
           intents: [<Button.Reset>Back</Button.Reset>],
         }
       : {
-          image: '/images/puzzle-frame.png',
+          image: (
+            <div style={container}>
+              <img
+                alt='puzzle'
+                src='/images/puzzle-frame.png'
+                width={'55%'}
+                height={'100%'}
+              />
+              {foundWords.includes('ethereum') ? (
+                <div style={{ ...highlight, ...highlightStyle.ethereum }}></div>
+              ) : (
+                ''
+              )}
+              {foundWords.includes('popcorn') ? (
+                <div style={{ ...highlight, ...highlightStyle.popcorn }}></div>
+              ) : (
+                ''
+              )}
+              {foundWords.includes('swap') ? (
+                <div style={{ ...highlight, ...highlightStyle.swap }}></div>
+              ) : (
+                ''
+              )}
+              {foundWords.includes('token') ? (
+                <div style={{ ...highlight, ...highlightStyle.token }}></div>
+              ) : (
+                ''
+              )}
+              {foundWords.includes('wallet') ? (
+                <div style={{ ...highlight, ...highlightStyle.wallet }}></div>
+              ) : (
+                ''
+              )}
+            </div>
+          ),
           intents: [
             <TextInput placeholder='Enter word...' />,
             <Button value='check'>Check word 🔎</Button>,
